380a40c9db4af2ff85d6183a657121627f44ea11
GerEugene <johnDoe.QAIsr@gmail.com>
2024-01-30 14:48:23 +0200

    Add new string in Branch_01

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,6 +11,8 @@ public class Main {
 
         System.out.printf("New string");
 
+        System.out.println("New string in Branch_01");
+
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
